6d276f291cefed6a72e9edb7f9db908aa7f181d1
Martin Balko <martin@balko.info>
2023-09-21 20:29:46 +0200

    Big update
    - Fix seat availability check
    - Fix set tariffs not being used in search endpoint
    - Refactor search_class() into search_train()
    - Refactor config loading (specifically working with tariffs)
    - Delete useless paramteres from search_endpoint
    - Add support for specifying maximum changes
    - Add support for looking into multiple trains with the same departure time

--- rjapi.py
+++ rjapi.py
@@ -8,25 +8,33 @@ class rjapi:
         if config_file is None:
             config_file = "config.yaml"
         self.config = self.__load_config(config_file)
-        self.__search_endpoint = "https://brn-ybus-pubapi.sa.cz/restapi/routes/search/simple?locale=sk&departureDate={}&fromLocationId={}&toLocationId={}&fromLocationType={}&toLocationType={}&tariffs=REGULAR"
-        self.__train_enpoint = "https://brn-ybus-pubapi.sa.cz/restapi/routes/{0}/simple?routeId={0}&fromStationId={1}&toStationId={2}&tariffs=REGULAR"
-        self.__shop_link = "https://regiojet.cz/?departureDate={}&fromLocationId={}&toLocationId={}&fromLocationType=CITY&toLocationType=CITY&tariffs={}"
+        self.__search_endpoint = "https://brn-ybus-pubapi.sa.cz/restapi/routes/search/simple?departureDate={}&fromLocationId={}&toLocationId={}&fromLocationType={}&toLocationType={}&tariffs={}"
+        self.__train_enpoint = "https://brn-ybus-pubapi.sa.cz/restapi/routes/{0}/simple?routeId={0}&fromStationId={1}&toStationId={2}&tariffs={3}"
+        self.__shop_link = "https://regiojet.cz/?departureDate={}&fromLocationId={}&toLocationId={}&fromLocationType={}&toLocationType={}&tariffs={}"
     
 
     def __load_config(self, config_file):
         with open(config_file, "r") as f:
             cfg = yaml.safe_load(f)
-        if type(cfg["tariff"]) is str:
-            cfg["tariff"] = [cfg["tariff"]]
-        cfg["quantity"] = len(cfg["tariff"])
+        if type(cfg["tariff"]) is list:
+            cfg["quantity"] = len(cfg["tariff"])
+            cfg["tariff"] = "&tariffs=".join(cfg["tariff"])
+        else:
+            cfg["quantity"] = 1
         return cfg
     
 
-    def search_class(self, train):
+    def search_train(self, train):
         # Get all classes info for given train
-        classes = requests.get(self.__train_enpoint.format(train["id"], train["departureStationId"], train["arrivalStationId"])).json()
+        train_details = requests.get(self.__train_enpoint.format(train["id"], train["departureStationId"], train["arrivalStationId"], self.config["tariff"])).json()
+        # Check max changes
+        if len(train_details["sections"]) > self.config["max_changes"] + 1:
+            return False
+        # No preffered class
+        if not self.config["preffered_class"]:
+            return True
         # Find preffered class and check availability
-        for i in classes["priceClasses"]:
+        for i in train_details["priceClasses"]:
             if i["seatClassKey"] == self.config["preffered_class"] and i["freeSeatsCount"] >= self.config["quantity"]:
                 return True
         return False
@@ -34,43 +42,44 @@ class rjapi:
 
     def search_ticket(self):
         # Get all routes for given date
-        trains = requests.get(self.__search_endpoint.format(self.config["date"], self.config["from"], self.config["to"], self.config["from_type"], self.config["to_type"])).json()
+        day_trains = requests.get(self.__search_endpoint.format(self.config["date"], self.config["from"], self.config["to"], self.config["from_type"], self.config["to_type"], self.config["tariff"])).json()
         # No trains on given trains
-        if "routes" not in trains:
+        if "routes" not in day_trains:
             return False
         
-        # Find train with given time
-        trains = trains["routes"]
+        # Find trains with given time
+        day_trains = day_trains["routes"]
         datetime = self.config["date"] + "T" + self.config["time"]
-        train = None
-        for i in trains:
+        trains = []
+        for i in day_trains:
             if datetime in i["departureTime"]:
-                train = i
-                break
+                trains.append(i)
         
-        # No train with given time
-        if train is None:
+        # No trains with given time
+        if not trains:
             return False
         
-        # No tickets available
-        if not train["freeSeatsCount"]:
-            return False
+        for train in trains:
+            # Not enough tickets available or train not bookable
+            if train["freeSeatsCount"] < self.config["quantity"] or not train["bookable"]:
+                continue
         
-        # Seat available and no preffered class
-        if not self.config["preffered_class"]:
+            # Seat available - max changes exceeded or preffered class not available
+            if not self.search_train(train):
+                continue
+
+            # Seat available
             return True
         
-        # Seat available but preffered class,
-        # check that class availability
-        return self.search_class(train)
+        # No train with seats available
+        return False
 
 
     def send_alert(self):
         # Craft data
-        tariffs = "&tariffs=".join(self.config["tariff"])
         data = {
             "message" : "Tickets for {}T{} available!".format(self.config["date"], self.config["time"]),
-            "action" : self.__shop_link.format(self.config["date"], self.config["from"], self.config["to"], tariffs)
+            "action" : self.__shop_link.format(self.config["date"], self.config["from"], self.config["to"], self.config["from_type"], self.config["to_type"], self.config["tariff"])
         }
         # Send to notify.run
-        requests.post("https://notify.run/"+self.config["notify_code"], data=data)
+        requests.post("https://notify.run/"+self.config["notify_code"], data=data)  
